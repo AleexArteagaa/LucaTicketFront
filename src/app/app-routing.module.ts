c379fa6fc0bcf5d89b8896d137a2dce91fa9e2d4
Jonay21 <106991498+Jonay21@users.noreply.github.com>
2023-12-26 16:09:08 +0100

    homee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EventosListComponent } from './components/eventos-list/eventos-list.component';
 import { UsuariosListComponent } from './components/Usuario/usuarios-list/usuarios-list.component';
 import { AltaUsuarioComponent } from './components/Usuario/alta-usuario/alta-usuario.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {
+		path: '/',
+		component: HomeComponent
+	},
   { 
     path: 'eventos', 
     component: EventosListComponent 
@@ -23,13 +28,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
-const routes: Routes = [
-	{
-		path: '/',
-		component: HomeComponent
-	},
-
-];
 
 export class AppRoutingModule { }
