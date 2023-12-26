c987ac835ac289cc65c97f94515a548e62e51908
AleexArteagaa <alejandroarteagaarias@gmail.com>
2023-12-26 10:48:26 +0100

    model

--- src/app/model/evento.ts
+++ src/app/model/evento.ts
@@ -1,2 +1,31 @@
+import { LocalDate, LocalTime } from 'js-joda';
+
 export class Evento {
+  id: number;
+  nombre: string;
+  descripcionCorta: string;
+  descripcionExtendida: string;
+  foto: string;
+  fechaEvento: LocalDate;
+  horaEvento: LocalTime;
+  precioMinimo: string;
+  precioMaximo: string;
+  normas: string;
+  recinto: string;
+
+
+  constructor() {
+    this.id = 0; // o cualquier otro valor por defecto
+    this.nombre = '';
+    this.descripcionCorta = '';
+    this.descripcionExtendida = '';
+    this.foto = '';
+    this.fechaEvento = LocalDate.now(); // o cualquier otra inicialización adecuada
+    this.horaEvento = LocalTime.now(); // o cualquier otra inicialización adecuada
+    this.precioMinimo = '';
+    this.precioMaximo = '';
+    this.normas = '';
+    this.recinto = '';
+  }
+
 }
--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,2 +1,19 @@
-export class Usuario {
+import { LocalDate } from 'js-joda'; // Asegúrate de importar la librería adecuada para manejar fechas en TypeScript
+
+export class UsuarioDTO {
+  id: number;
+  nombre: string;
+  apellido: string;
+  mail: string;
+  contrasenia: string;
+  fechaAlta: LocalDate;
+
+  constructor() {
+    this.id = 0; // o cualquier otro valor por defecto
+    this.nombre = '';
+    this.apellido = '';
+    this.mail = '';
+    this.contrasenia = '';
+    this.fechaAlta = LocalDate.now();
+  }
 }
