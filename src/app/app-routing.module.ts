11a28300d22bf238fa34af9f3b521f15096a8bf2
AleexArteagaa <alejandroarteagaarias@gmail.com>
2023-12-26 13:49:48 +0100

    listado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventosListComponent } from './components/eventos-list/eventos-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'eventos', 
+    component: EventosListComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/eventos-list/eventos-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { Evento } from '../../model/evento';
+import { EventosService } from '../../service/eventos.service';
+
+@Component({
+  selector: 'app-eventos-list',
+  templateUrl: './eventos-list.component.html',
+  styleUrl: './eventos-list.component.css'
+})
+
+
+
+export class EventosListComponent implements OnInit{
+
+  eventos: Evento[] = [];
+
+
+  constructor(private router: Router, private eventosService: EventosService) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.eventosService.getEventos()
+      .subscribe(data => {
+        this.eventos = data;
+      });
+  };
+
+}
