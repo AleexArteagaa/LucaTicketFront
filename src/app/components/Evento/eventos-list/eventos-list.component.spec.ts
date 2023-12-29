51c85b24f0ded3b89f174091f132f50c1cd6b825
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-29 12:04:54 +0100

    tests

--- src/app/components/Evento/eventos-list/eventos-list.component.spec.ts
+++ src/app/components/Evento/eventos-list/eventos-list.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventosListComponent } from './eventos-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EventosListComponent', () => {
   let component: EventosListComponent;
@@ -13,7 +14,7 @@ describe('EventosListComponent', () => {
       declarations: [EventosListComponent],
       imports: [
         RouterTestingModule,
-        HttpClientModule
+        HttpClientTestingModule
       ],
     })
     .compileComponents();
