9b747766f80d4d1121a7a4570e16669e711b82d8
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-29 12:03:09 +0100

    tests

--- src/app/components/Entradas/entradas-compra/entradas-compra.component.spec.ts
+++ src/app/components/Entradas/entradas-compra/entradas-compra.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EntradasCompraComponent } from './entradas-compra.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EntradasCompraComponent', () => {
   let component: EntradasCompraComponent;
@@ -10,7 +11,8 @@ describe('EntradasCompraComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EntradasCompraComponent],
-      imports: [ 
+      imports: [
+        RouterTestingModule,
         HttpClientTestingModule
       ],
     })
